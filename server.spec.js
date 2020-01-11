42bf6f311efd7b468dfeb0da28eb9f186f7d4181
Tyler Spaulding <TylerSpaulding95@gmail.com>
2020-01-11 00:00:04 -0500

    Adding testing for server routes

--- server.spec.js
+++ server.spec.js
@@ -1,3 +1,29 @@
 const server = require('./server')
-const request = require('supertest')
+const supertest = require('supertest')
 
+//TESTING RETURN OF /ALL AFTER ASYNC OP
+describe("GET /all", function() {
+    it("should return a 200 OK", async function() {
+      const result = await server.results;
+      if(result) {
+      return supertest(server)
+        .get("/all")
+        .then(res => {
+          expect(res.status).toBe(200);
+        });
+      };
+    });
+});
+// TESTING RETURN OF /COMEDIANS AFTER ASYNC OP
+describe("GET /comedians", function() {
+    it('should return 200 OK', async function() {
+        const names = await server.names
+        if(names) {
+            return supertest(server)
+            .get("/comedians")
+            .then((res) => {
+                expect(res.status).toBe(200)
+            });
+        };
+    });
+});
